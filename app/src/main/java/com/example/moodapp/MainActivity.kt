0b743e59eeee45442466ac22c8dd7213390cd694
40152370 <jhagan06@qub.ac.uk>
2021-09-08 01:23:19 +0100

    homepage design

--- app/src/main/java/com/example/moodapp/MainActivity.kt
+++ app/src/main/java/com/example/moodapp/MainActivity.kt
@@ -42,8 +42,6 @@ class MainActivity : AppCompatActivity() {
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home,
-                R.id.navigation_mood_library,
-                R.id.navigation_mood_boost,
                 R.id.navigation_mood_music
             )
         )
