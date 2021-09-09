60553d233f55d586be1e5af827ab2158171d8a0d
40152370 <jhagan06@qub.ac.uk>
2021-09-09 13:51:09 +0100

    Some UI changes

--- app/src/main/java/com/example/moodapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/moodapp/ui/home/HomeViewModel.kt
@@ -7,11 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "Welcome to Moodfy. Where moods and music meet." +
-                " Making playlists based on moods just became that much easier!" +
-                " To get started simply hit START MOODFYING YOUR MUSIC and" +
-                " the Moodfy bubble will allow you yo listen to music in Spotify and easily create mood playlists " +
-                " at the click of a button"
+        value = "Moodfy. Where Mood meets music."
     }
     val text: LiveData<String> = _text
 }
