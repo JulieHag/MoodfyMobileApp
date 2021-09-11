29480bfb4313450598fe998b1482fac8afb38524
40152370 <jhagan06@qub.ac.uk>
2021-09-11 19:42:41 +0100

    Resourse wrapper for api calls in viewmodel

---
+++ app/src/main/java/com/example/moodapp/utils/Resource.kt
@@ -0,0 +1,17 @@
+package com.example.moodapp.utils
+
+/**
+ * Sealed class - Can define which classes are allowed to inherit from Resource
+ */
+sealed class Resource<T>(
+    val data: T? = null,
+    val message: String? = null
+) {
+    //data not nullable here because if successful then body cannot be null
+    class Success<T>(data: T) : Resource<T>(data)
+
+    //data nullable here because can have a body in an error response but not always
+    class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
+
+}
+
