f0812144c0fac9236aef5847e1c912d7d0f6a09f
40152370 <jhagan06@qub.ac.uk>
2021-09-26 16:54:02 +0100

    minor dialog changes

--- app/src/main/java/com/jhag/moodapp/ui/moodMusic/MoodMusicFragment.kt
+++ app/src/main/java/com/jhag/moodapp/ui/moodMusic/MoodMusicFragment.kt
@@ -16,9 +16,9 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.jhag.moodapp.databinding.FragmentMoodMusicBinding
 import com.jhag.moodapp.ui.floatingIcon.MoodIconService
+import com.jhag.moodapp.ui.viewmodels.MoodMusicViewModel
 import com.jhag.moodapp.utils.Constants
 import com.jhag.moodapp.utils.SessionManager
-import com.jhag.moodapp.ui.viewmodels.MoodMusicViewModel
 import com.spotify.sdk.android.authentication.AuthenticationClient
 import com.spotify.sdk.android.authentication.AuthenticationRequest
 import com.spotify.sdk.android.authentication.AuthenticationResponse
@@ -153,7 +153,7 @@ class MoodMusicFragment : Fragment() {
         with(builder)
         {
             setTitle("Permission required")
-            setMessage("To use this feature of the app you must allow it to display over other apps. Enable this manually in your phone's settings")
+            setMessage("To use this feature of the app you must allow it to display over other apps. Enable this manually in your phone's settings.")
             setPositiveButton(
                 "OK",
                 DialogInterface.OnClickListener(function = positiveButtonClick)
@@ -164,7 +164,7 @@ class MoodMusicFragment : Fragment() {
 
     }
 
-    fun spotifyAccessAlert() {
+    private fun spotifyAccessAlert() {
 
 
         val builder = AlertDialog.Builder(requireContext())
