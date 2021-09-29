a6b1e830d5ae6b64f21f0fe31d6dafda6da309a6
40152370 <jhagan06@qub.ac.uk>
2021-09-29 07:26:39 +0100

    comments

--- app/src/main/java/com/jhag/moodapp/ui/MainActivity.kt
+++ app/src/main/java/com/jhag/moodapp/ui/MainActivity.kt
@@ -21,6 +21,9 @@ import com.spotify.sdk.android.authentication.AuthenticationClient
 import com.spotify.sdk.android.authentication.AuthenticationRequest
 import com.spotify.sdk.android.authentication.AuthenticationResponse
 
+/**
+ * Main activity which hosts the fragments
+ */
 
 class MainActivity : AppCompatActivity() {
 
